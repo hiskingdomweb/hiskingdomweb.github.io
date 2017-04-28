6f114623aae7973aa3d39bb3cf4af3e6f480ccaf
Jon Boyce <jsboyce@sep.com>
2017-04-28 14:32:52 -0400

    Nuking and blind-replacing with stuff I got in a zip file.

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,72 @@
+$(document).ready(function($){
+
+/*------------------------------------------
+	 Navigation
+-------------------------------------------*/
+var open = 0;
+
+$('.menuButton').bind('click', function(){
+  $('.btn').toggleClass('activeBtn');
+  if (open===0) {
+          $("#overmenu").animate({"margin-right": '+=101%'});
+          $('.wrapper').addClass("fixedPosition");
+          open=1;
+        } else {
+          $("#overmenu").animate({"margin-right": '-=101%'});
+		  $('.wrapper').removeClass("fixedPosition");
+          open=0;
+        }
+})
+
+$("#overmenu").on("click", "a", function(event){
+   $('.btn').toggleClass('activeBtn');
+        if (open===0) {
+          $("#overmenu").animate({"margin-right": '+=101%'});
+          open=1;
+        } else {
+          $("#overmenu").animate({"margin-right": '-=101%'});
+          open=0;
+        }
+});
+
+
+// var offset = $('.video-btn').offset().top;
+// var navbar = $('.header');
+//
+// $(document).scroll(function() {
+// 	position = $(this).scrollTop();
+//   if (position < offset)
+//   	navbar.css({'background-color': 'rgba(255, 255, 255, 0)', 'box-shadow': '0px 0px 0px 0px rgba(255, 255, 255, 0)'});
+//   else
+//     navbar.css({'background-color': '#fff', 'box-shadow': '0px 2px 10px 0px rgba(88, 82, 71, 0.25)'});
+// });
+
+var offset = $('.video-btn').offset().top;
+var navbar = $('.header');
+
+$(document).scroll(function() {
+	position = $(this).scrollTop();
+  if (position < offset)
+  	navbar.removeClass("header-solid");
+  else
+		navbar.addClass("header-solid");
+});
+
+
+/*------------------------------------------
+	 Big Banner
+-------------------------------------------*/
+$(document).ready(function() {
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    $('.banner-c').css('height', windowHeight);
+  };
+  setHeight();
+
+  $(window).resize(function() {
+    setHeight();
+  });
+});
+
+
+});
