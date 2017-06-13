701cbb9729675259ecc39ebea6245e1cf29b2413
tmorin41 <tmorin41@gmail.com>
2017-06-13 12:16:21 -0400

    Add files via upload

--- email.php
+++ email.php
@@ -1,16 +1,13 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$phone = $_POST['phone'];
-$call = $_POST['call'];
-$website = $_POST['website'];
-$priority = $_POST['priority'];
-$type = $_POST['type'];
+$dropdown = $POST['dropdown'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
-$recipient = "tmorin41@gmail.com";
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Go Back</a>";
+
 ?>
