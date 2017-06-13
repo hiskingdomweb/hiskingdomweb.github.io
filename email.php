4042869c8cd11746980e49de9ef74f58563e2d0d
tmorin41 <tmorin41@gmail.com>
2017-06-13 12:14:30 -0400

    Add files via upload

--- email.php
+++ email.php
@@ -12,5 +12,5 @@ $recipient = "tmorin41@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
