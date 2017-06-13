304b4b62dd4ecf79235364d09503657fc2397a1c
tmorin41 <tmorin41@gmail.com>
2017-06-13 12:08:15 -0400

    Add files via upload

--- email.php
+++ email.php
@@ -1,36 +1,11 @@
-<?php
-
-$EmailFrom = "chriscoyier@gmail.com";
-$EmailTo = "tmorin41@gmail.com";
-$Subject = "Nice & Simple Contact Form by CSS-Tricks";
-$Cat = Trim(stripslashes($_POST['Category'])); 
-$Name = Trim(stripslashes($_POST['Name'])); 
-$Email = Trim(stripslashes($_POST['Email'])); 
-$Message = Trim(stripslashes($_POST['Message'])); 
-
-// validation
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
-
-// prepare email body text
-$Body = "";
-$Body .= "Category: ";
-$Body .= $Cat;
-$Body .= "\n";
-$Body .= "Name: ";
-$Body .= $Name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $Email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $Message;
-$Body .= "\n";
-
-// send email 
-$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
-
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$dropdown = $POST['dropdown'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "tmorin41@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
