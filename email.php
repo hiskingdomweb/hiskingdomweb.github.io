bec6ed903d0c4370cd31f9a784d9a87bb7c83ccf
tmorin41 <tmorin41@gmail.com>
2017-06-13 14:43:33 -0400

    Add files via upload

--- email.php
+++ email.php
@@ -1,43 +1,12 @@
 <?php
-
-$EmailFrom = "chriscoyier@gmail.com";
-$EmailTo = "tmorin41@gmail.com";
-$Subject = "Nice & Simple Contact Form by CSS-Tricks";
-$Cat = Trim(stripslashes($_POST['Category'])); 
-$Name = Trim(stripslashes($_POST['Name'])); 
-$Email = Trim(stripslashes($_POST['Email'])); 
-$Message = Trim(stripslashes($_POST['Message'])); 
-
-// validation
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
-
-// prepare email body text
-$Body = "";
-$Body .= "Category: ";
-$Body .= $Cat;
-$Body .= "\n";
-$Body .= "Name: ";
-$Body .= $Name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $Email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $Message;
-$Body .= "\n";
-
-// send email 
-$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
-
-// redirect to success page 
-if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
-}
-else{
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$type = $_POST['type'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Type: $type \n Message: $message";
+$recipient = "tmorin41@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
